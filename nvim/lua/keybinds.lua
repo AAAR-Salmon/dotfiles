56acf7efed994aa2546bc8ae0b9d288f286ea63a
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-11-20 19:11:18 +0900

    change (nvim): disable jk in visual mode

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,4 +1,5 @@
 local map = vim.api.nvim_set_keymap
 map('i', 'jk', '<ESC>', {noremap = true})
-map('v', 'jk', '<ESC>', {noremap = true})
+-- Disable this since `jjj...jk` is common in visual line mode
+-- map('v', 'jk', '<ESC>', {noremap = true})
 map('v', 'q', '<ESC>', {noremap = true})
