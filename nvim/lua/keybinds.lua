31ba411eb01ae7a903cc0696a6348385054a4cb2
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-06-06 12:19:26 +0900

    refactor (nvim): use vim.keymap.set() instead vim.api.nvim_set_keymap()
    default noremap

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,5 +1,5 @@
-local map = vim.api.nvim_set_keymap
-map('i', 'jk', '<ESC>', {noremap = true})
+local map = vim.keymap.set
+map('i', 'jk', '<ESC>')
 -- Disable this since `jjj...jk` is common in visual line mode
--- map('v', 'jk', '<ESC>', {noremap = true})
-map('v', 'q', '<ESC>', {noremap = true})
+-- map('v', 'jk', '<ESC>')
+map('v', 'q', '<ESC>')
