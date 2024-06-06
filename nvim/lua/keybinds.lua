d4f7b72d60d156589433bba6d2b1beba59c81ce5
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-06-06 12:19:58 +0900

    add (nvim): keymap g=, G= for ls formatting

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -3,3 +3,5 @@ map('i', 'jk', '<ESC>')
 -- Disable this since `jjj...jk` is common in visual line mode
 -- map('v', 'jk', '<ESC>')
 map('v', 'q', '<ESC>')
+map('n', 'g=', function() vim.lsp.buf.format() end)
+map('n', 'G=', 'g=', { remap = true })
