548ac852baf22a2640aefda8d8a1eae77f83309e
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-07-24 11:49:02 +0900

    fix (zsh): auto detect filetype yaml.docker-compose

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -30,3 +30,12 @@ opt.fileencodings = { 'utf-8', 'sjis' }
 opt.helplang = 'ja', 'en'
 
 vim.cmd('set formatexpr=autofmt#japanese#formatexpr()')
+
+vim.filetype.add({
+  filename = {
+    ['compose.yaml'] = 'yaml.docker-compose',
+    ['compose.yml'] = 'yaml.docker-compose',
+    ['docker-compose.yaml'] = 'yaml.docker-compose',
+    ['docker-compose.yml'] = 'yaml.docker-compose',
+  },
+})
