01421686c184cac95a0f25ca72c61d3351b11dbf
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-08-01 14:42:02 +0900

    add (nvim): telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -35,6 +35,19 @@ require('jetpack.packer').add {
   { 'hrsh7th/cmp-cmdline' },
   { 'hrsh7th/cmp-path' },
   -- Manupilation
+  {
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end,
+  },
   { 'ervandew/supertab' },
   { 'machakann/vim-sandwich' },
   -- Filer
