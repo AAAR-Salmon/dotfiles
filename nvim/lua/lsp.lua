2f24123437ff50a29aad04977ce674272c0ca1d6
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-08-01 17:36:50 +0900

    change (nvim): disable diagnostics of LSP on virtual text and show by hold

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -8,3 +8,33 @@ require('mason-lspconfig').setup_handlers {
     }
   end,
 }
+
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local buffer = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    -- a
+    vim.o.updatetime = 500
+    vim.api.nvim_create_autocmd("CursorHold", {
+      buffer = bufnr,
+      callback = function()
+        local opts = {
+          focusable = false,
+          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+          border = 'rounded',
+          source = 'always',
+          prefix = ' ',
+          scope = 'cursor',
+        }
+        vim.diagnostic.open_float(nil, opts)
+      end,
+    })
+  end,
+})
