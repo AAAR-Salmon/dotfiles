dc98a96e032ebcb81ba14753cf5a62ac40e1564d
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-08-01 14:59:33 +0900

    change (nvim): keymap by which-key

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,10 +1,40 @@
-local map = vim.keymap.set
-map('i', 'jk', '<ESC>')
--- Disable this since `jjj...jk` is common in visual line mode
--- map('v', 'jk', '<ESC>')
-map('v', 'q', '<ESC>')
-map('n', 'g=', function() vim.lsp.buf.format() end)
-map('n', 'G=', 'g=', { remap = true })
-map('n', 'gd', function() vim.lsp.buf.definition() end)
-map('n', 'gr', function() vim.lsp.buf.rename() end)
-map('n', 'gu', function() vim.lsp.buf.references() end)
+local wk = require('which-key')
+
+vim.g.mapleader = ','
+wk.add({
+  { 'jk', '<esc>', mode = 'i' },
+  { 'q',  '<esc>', mode = 'v' },
+  {
+    'g=',
+    function()
+      vim.lsp.buf.format()
+    end,
+    mode = 'n',
+    desc = 'Format by Language Server',
+  },
+  { 'G=', 'g=', mode = 'n', remap = true },
+  {
+    'gd',
+    function()
+      vim.lsp.buf.definition()
+    end,
+    mode = 'n',
+    desc = 'Jump to definition by Language Server',
+  },
+  {
+    'gr',
+    function()
+      vim.lsp.buf.rename()
+    end,
+    mode = 'n',
+    desc = 'Rename variable by Language Server',
+  },
+  {
+    'gu',
+    function()
+      vim.lsp.buf.references()
+    end,
+    mode = 'n',
+    desc = 'Jump to references by Language Server',
+  },
+})
