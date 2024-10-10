f4fa9f8d11af366cb98498022b8b57233842d08f
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2024-10-10 19:48:36 +0900

    fix (nvim): nvim-cmp setup did not use some sources

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,6 +1,13 @@
 return {
   {
     'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-emoji',
+    },
     config = function()
       local cmp = require('cmp')
       cmp.setup({
@@ -17,24 +24,29 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-        }, {
-          { name = 'buffer' }
-        }),
+        sources = cmp.config.sources(
+          {
+            { name = 'nvim_lsp' },
+          },
+          {
+            { name = 'buffer' },
+            { name = 'path' },
+            { name = 'emoji' },
+          }
+        ),
+      })
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+      cmp.setup.cmdline(':',{
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'cmdline' },
+        },
       })
     end,
-  },
-  {
-    'hrsh7th/cmp-nvim-lsp',
-  },
-  {
-    'hrsh7th/cmp-buffer',
-  },
-  {
-    'hrsh7th/cmp-cmdline',
-  },
-  {
-    'hrsh7th/cmp-path',
   },
 }
